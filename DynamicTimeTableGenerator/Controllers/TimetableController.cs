e3cfceb95c98f9ea93253294eb8379eaef521e07
dipalikuhad1234 <dipalikuhad2910@gmail.com>
2025-03-08 14:05:12 -0600

    Dynamic-timetable-generate

---
+++ DynamicTimeTableGenerator/Controllers/TimetableController.cs
@@ -0,0 +1,78 @@
+﻿using DynamicTimeTableGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicTimeTableGenerator.Controllers
+{
+    public class TimetableController : Controller
+    {
+        public IActionResult CreateTimetable() => View();
+
+        public IActionResult AllocateSubjects(int workingDays, int subjectsPerDay, int totalSubjects)
+        {
+            var model = new TimeTable
+            {
+                WorkingDays = workingDays,
+                SubjectsPerDay = subjectsPerDay,
+                TotalSubjects = totalSubjects,
+                Subjects = Enumerable.Repeat(new SubjectHours(), totalSubjects).ToList()
+            };
+            if (TempData.ContainsKey("ErrorMessage"))
+            {
+                ViewBag.ErrorMessage = TempData["ErrorMessage"];
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ViewTimetable(TimeTable model)
+        {
+            int allocatedHours = model.Subjects.Sum(s => s.Hours);
+
+            if (allocatedHours != model.TotalHours)
+            {
+                TempData["ErrorMessage"] = $"Total subject hours ({allocatedHours}) must match Total Hours for the Week ({model.TotalHours}).";
+
+                return RedirectToAction("AllocateSubjects", new
+                {
+                    workingDays = model.WorkingDays,
+                    subjectsPerDay = model.SubjectsPerDay,
+                    totalSubjects = model.TotalSubjects
+                });
+            }
+
+            model.Timetable = GenerateTimetableGrid(model);
+            return View("ViewTimetable", model);
+        }
+
+        private List<List<string>> GenerateTimetableGrid(TimeTable model)
+        {
+            var subjectList = model.Subjects
+                .SelectMany(s => Enumerable.Repeat(s.SubjectName, s.Hours))
+                .ToList();
+
+            var rand = new Random();
+            var timetable = new List<List<string>>();
+
+            for (int i = 0; i < model.SubjectsPerDay; i++)
+            {
+                var row = new List<string>();
+
+                for (int j = 0; j < model.WorkingDays; j++)
+                {
+                    if (subjectList.Any())
+                    {
+                        int index = rand.Next(subjectList.Count);
+                        row.Add(subjectList[index]);
+                        subjectList.RemoveAt(index);
+                    }
+                    else
+                    {
+                        row.Add(string.Empty);
+                    }
+                }
+                timetable.Add(row);
+            }
+            return timetable;
+        }
+    }
+}
---
+++ DynamicTimeTableGenerator/Models/TimeTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace DynamicTimeTableGenerator.Models
+{
+    public class TimeTable
+    {
+        [Range(1, 7, ErrorMessage = "Working days must be between 1 and 7.")]
+        public int WorkingDays { get; set; }
+
+        [Range(1, 8, ErrorMessage = "Subjects per day must be between 1 and 8.")]
+        public int SubjectsPerDay { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Total subjects must be greater than 0.")]
+        public int TotalSubjects { get; set; }
+        public int TotalHours => WorkingDays * SubjectsPerDay;
+
+        public int TotalAllocatedHours => Subjects.Sum(s => s.Hours);
+
+        public List<SubjectHours> Subjects { get; set; } = new();
+
+        public List<List<string>> Timetable { get; set; } = new();
+        public bool IsValid() => TotalAllocatedHours == TotalHours;
+    }
+
+    public class SubjectHours
+    {
+        [Required(ErrorMessage = "Subject name is required.")]
+        public string SubjectName { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Hours must be a positive number.")]
+        public int Hours { get; set; }
+    }
+}
